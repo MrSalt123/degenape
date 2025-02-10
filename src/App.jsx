0767f6aba1507c6e403fff30cc22c2086982a090
MrSalt123 <njn04807@uga.edu>
2025-02-10 18:54:04 -0500

    bg change

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import { Parallax, ParallaxLayer } from '@react-spring/parallax';
 import Card from 'react-bootstrap/Card';
 import phantom from './assets/images/phantom.svg';
 import solana from './assets/images/solana.avif';
 import swap from './assets/images/swap.svg';
+import layer1 from './assets/images/layer1.png';
+import layer2 from './assets/images/layer2.png';
+import layer3 from './assets/images/layer3.png';
 
 function App() {
-  const windowWidth = useState(window.innerWidth);
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
+
+  useEffect(() => {
+    console.log(isMobile);
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 768);
+    };
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
 
   return (
     <div>
@@ -68,10 +81,21 @@ function App() {
 
 
         {/* PIXEL MONKEY LAYERS */}
-        <ParallaxLayer offset={1} speed={.25}>
-          <div className="w-full h-full layer1">
-          </div>
-        </ParallaxLayer>
+        {isMobile ? (
+          <ParallaxLayer offset={1} speed={.2}>
+            <div className="absolute top-0 left-0 w-full h-full flex justify-center">
+              <p className="mt-5 buy-text text-4xl font-bold">How to Buy</p>
+            </div>
+          </ParallaxLayer>
+        ) : (
+          <ParallaxLayer offset={1} speed={0.25}>
+            <div className="w-full h-full flex flex-col justify-center">
+              <img className='w-full h-1/3' src={layer1}/>
+              <img className='w-full h-1/3' src={layer2}/>
+              <img className='w-full h-1/3' src={layer3}/>
+            </div>
+          </ParallaxLayer>
+        )}
 
 
         <ParallaxLayer offset={1} speed={.35}>
@@ -81,7 +105,7 @@ function App() {
                 <Card.Img variant="top" src={phantom} />
                 <Card.Body className="d-flex flex-column flex-grow-1">
                   <Card.Title className="card-title text-center">Set up a wallet</Card.Title>
-                  <Card.Text className="flex-grow-1 text-center">
+                  <Card.Text className="flex-grow-1 text-center card-text">
                     Download a Solana-compatible wallet (Phantom, Solflare, Sollet, etc.).
                   </Card.Text>
                 </Card.Body>
@@ -91,7 +115,7 @@ function App() {
                 <Card.Img variant="top" src={solana} />
                 <Card.Body className="d-flex flex-column flex-grow-1">
                   <Card.Title className="card-title text-center">Buy SOL</Card.Title>
-                  <Card.Text className="flex-grow-1 text-center">
+                  <Card.Text className="flex-grow-1 text-center card-text">
                     Acquire SOL from a supported exchange (Coinbase, Binance, etc.) and send SOL to your wallet
                   </Card.Text>
                 </Card.Body>
@@ -101,7 +125,7 @@ function App() {
                 <Card.Img variant="top" src={swap} />
                 <Card.Body className="d-flex flex-column flex-grow-1">
                   <Card.Title className="card-title text-center">Swap SOL for DAPE</Card.Title>
-                  <Card.Text className="flex-grow-1 text-center">
+                  <Card.Text className="flex-grow-1 text-center card-text">
                     Copy the CA below and then swap SOL for DAPE either through a DEX or directly in your wallet.
                   </Card.Text>
                 </Card.Body>
@@ -116,7 +140,7 @@ function App() {
           speed={.5}
         >
           <div className='flex justify-center absolute bottom-1/12 w-full'>
-            <p className='ca-text'>CA: NOT OUT YET</p>
+            <p className='ca-text drop-shadow-[2px_2px_0px_white]'>CA: NOT OUT YET</p>
           </div>
         </ParallaxLayer>
 
